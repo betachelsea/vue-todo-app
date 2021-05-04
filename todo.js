de1932c8dd0944c14589e33e5373195b71e11c61
betachelsea <beta.chelsea@gmail.com>
2021-05-04 12:21:07 +0900

    TODOの追加機能を作成

--- todo.js
+++ todo.js
@@ -1,4 +1,18 @@
 var app1 = new Vue({
   el: '#app',
-  data: { message: 'Hello world!' }
+  data: {
+    newTodoText: '',
+    todos: [], // (Ex) { key: 'j6fdcq68', body: 'todo body!' }
+  },
+  methods: {
+    addNewTodo: function() {
+      if (!this.newTodoText) { return; }
+
+      this.todos.push({
+        key: Math.random().toString(36).slice(-8),
+        body: this.newTodoText,
+      })
+      this.newTodoText = ''
+    }
+  }
 })
