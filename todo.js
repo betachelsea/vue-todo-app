09fe5dccec4ad8945d21983a4160569552f5beff
betachelsea <beta.chelsea@gmail.com>
2021-05-05 15:23:32 +0900

    keyとbodyを更新する箇所を共通化

--- todo.js
+++ todo.js
@@ -3,7 +3,7 @@ const itemKey = 'vueTodo'
 var app1 = new Vue({
   el: '#app',
   data: {
-    newTodoText: '', // これ要るか？
+    newTodoText: '',
     todos: {}, // (Ex) { "j6fdcq68": { "body": "Buy milk", "editing": true } }
   },
   created: function() {
@@ -16,17 +16,18 @@ var app1 = new Vue({
       if (!this.newTodoText) { return; }
 
       let key = Math.random().toString(36).slice(-8)
-      this.todos[key] = { body: this.newTodoText, editing: false }
+      this.upsertTodo(key, this.newTodoText)
       this.newTodoText = ''
-
-      localStorage.setItem(itemKey, JSON.stringify(this.todos))
     },
     edit: function(key) {
       this.todos[key].editing = true
     },
     update: function(e) {
       let el = e.target.elements
-      this.$set(this.todos, el.key.value, { body: el.body.value, editing: false })
+      this.upsertTodo(el.key.value, el.body.value)
+    },
+    upsertTodo: function(key, body) {
+      this.$set(this.todos, key, { body: body, editing: false })
       localStorage.setItem(itemKey, JSON.stringify(this.todos)) // TODO: 共通化
     }
   }
