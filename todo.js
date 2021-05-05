6fd1e535c3bd72626d5236cadf517d8d4c6c1512
betachelsea <beta.chelsea@gmail.com>
2021-05-05 13:47:24 +0900

    localStorage経由でjsonを保存/読込する

--- todo.js
+++ todo.js
@@ -1,8 +1,13 @@
+const itemKey = 'vueTodo'
+
 var app1 = new Vue({
   el: '#app',
   data: {
     newTodoText: '',
     todos: [], // (Ex) { key: 'j6fdcq68', body: 'todo body!' }
+  },
+  created: function() {
+    this.todos = JSON.parse(localStorage.getItem(itemKey))
   },
   methods: {
     addNewTodo: function() {
@@ -13,6 +18,7 @@ var app1 = new Vue({
         body: this.newTodoText,
       })
       this.newTodoText = ''
+      localStorage.setItem(itemKey, JSON.stringify(this.todos))
     }
   }
 })
